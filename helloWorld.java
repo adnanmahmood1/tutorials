759e49783251dc42dbad58dca28b434c01d8b55b
Adnan Mahmood <adnan.mahmood1@hotmail.com>
2016-09-30 22:54:04 +0100

    Change int variable name

--- helloWorld.java
+++ helloWorld.java
@@ -3,4 +3,12 @@ public class helloWorld {
     public static void main (String[] args){
         System.out.println("Hello World!");
     }
+
+
+public static int addOperation(int a, int b){
+    int totalAmount;
+    total = a+b;  
+    return total; 
+
+    } 
 }
