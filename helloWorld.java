574a4452e62960efea02b8a8feba3aa97868af48
Adnan Mahmood <adnan.mahmood1@hotmail.com>
2016-10-01 00:39:54 +0100

    more new stuff

--- helloWorld.java
+++ helloWorld.java
@@ -1,19 +1,18 @@
-public class helloWorld {
+public class HelloWorld {
+    int total; 
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); 
+         HelloWorld programm = new HelloWorld();
+        System.out.println(programm.addOperations(5,6)); 
+       
+    }   
 
-    public static void main (String[] args){
-        System.out.println("Hello World!");
+    public  int addOperations(int a, int b) {
+        total = a+b;
+        return total; 
+    }   
+
+    public int minusOperations(int a, int b){
+        //null 
     }
-
-
-public static int addOperation(int a, int b){
-    int totalAmount;
-    total = a+b;  
-    return total; 
-
-    } 
-public static int minusOperation(int a, int b) {
-    int totalAmount;
-    total = a-b; 
-    return total; 
-} 
 }
